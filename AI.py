1d011f141bd15f08668f833575dbf3c62290e9a7
Wiz8910 <adamconaldbowers@gmail.com>
2014-10-10 15:21:01 -0500

    Update AI.py
    added random move function

--- AI.py
+++ AI.py
@@ -45,7 +45,12 @@ class AI(BaseAI):
   ##This function is called once, after your last turn
   def end(self):
     pass
-
+  def move_units(self):
+    for droid in self.droids
+    directions = [(1,0), (-1,0),(0,1),(0,-1)]
+    for _ in range(self.maxMovement):
+        movement = random.choice(direction)
+    droid.move(droid.x+movement[0], droid.y+movement[1])
   ##This function is called each time it is your turn
   ##Return true to end your turn, return false to ask the server for updated information
   def run(self):
